4e29ecd7c4bdb8b7c5064c8096c4eb810adc47c8
janghendar <suhendarweb@gmail.com>
2020-11-01 13:24:51 +0700

    Proses Debugging

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/hello', function () {
-    return 'Selamat Datang di laravel';
-});
+// Route::get('/hello', function () {
+//     return 'Selamat Datang di laravel';
+// });
 
 Route::get('/belajar', function() {
     echo '<h1>Hello World</h1>';
@@ -35,13 +35,13 @@ Route::get('/mahasiswa/fasilkom/anto', function() {
 
 });
 
-// Route Parameter
+# Route Parameter
 Route::get( '/mahasiswa/{nama}', function($nama) {
     return "Tampilkan mahasiswa bernama $nama";
 
 });
 
-// Router dengan beberapa parameter
+# Router dengan beberapa parameter
 Route::get( '/stok_barang/{jenis}/{merek}', function($jenis, $merek){
     return "Cetak sisa stok untuk $jenis $merek";
 
@@ -66,3 +66,79 @@ Route::get('user/{id}', function($id) {
     return "Tampilkan user dengan id = $id";
 
 }) -> where('id', '[0-9]+');
+
+Route::get('user/{id}', function($id) {
+    return "Tampilkan user dengan id = $id";
+
+}) -> where('id', '[A-Z]{2}[0-9]+');
+
+# Route Redirect
+
+Route::get( '/hubungi-kami', function() {
+    return "<h1>Hubungi Kami</h1>";
+});
+
+Route::redirect('/contact-us', '/hubungi-kami');
+
+#Route Group
+
+Route::prefix ('/admin') -> group(function() {
+
+    Route::get('/mahasiswa', function () {
+        echo "<h1>Daftar Mahasiswa </h1>";
+    });
+
+    Route::get('/dosen', function () {
+        echo "<h1>Daftar Dosen </h1>";
+    });
+
+    Route::get('/karyawan', function () {
+        echo "<h1>Daftar Karyawan </h1>";
+    });
+
+});
+
+# Route Fallback
+
+Route::fallback(function () {
+    return "Maaf alamat yang anda cari tidak ditemukan";
+});
+
+# Route priority
+
+Route::get('/buku/{a}', function ($a) {
+    return "Buku ke $a";
+});
+
+Route::get('/buku/{b}', function ($b) {
+    return "Buku ke $b";
+});
+
+Route::get('/buku/{c}', function ($c) {
+    return "Buku ke $c";
+});
+
+# Proses Debugging dan Error Display
+
+Route::get('/hello', function () {
+
+    $hello = "Hello World";
+    var_dump($hello);
+    die();
+    return 'Selamat Datang di laravel';
+});
+
+Route::get('/hello', function () {
+
+    $hello = "Hello World";
+    dd($hello);
+    return 'Selamat Datang di laravel';
+});
+
+Route::get('/hello', function () {
+
+    $hello = ['Hello World', 2 => ['Hello jakarta', 'Hello Medan']];
+    dd($hello);
+
+    return 'Selamat Datang di laravel';
+});
