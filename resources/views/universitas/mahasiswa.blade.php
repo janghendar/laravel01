6e4756224824da4841d173e6786f697cda3c8e6a
janghendar <suhendarweb@gmail.com>
2020-11-05 20:04:56 +0700

    Bootstrap

--- resources/views/universitas/mahasiswa.blade.php
+++ resources/views/universitas/mahasiswa.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman View</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -37,10 +37,10 @@
     
     </ul>
 
-    <div>
+    <!-- <div>
     <img class="rounded-circle img-thumbnail m-2" src="/img/foto1.jpg">
     <img class="rounded-circle img-thumbnail m-2" src="/img/foto2.jpg" >
-    </div>
+    </div> -->
     <p>
     <div>
         Copyright <?php echo date("M-Y"); ?>. Suhendar Aryadi
@@ -48,8 +48,8 @@
 
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
