00bf3db6de5f55cc4fa5317ed00e18624b13fa4a
janghendar <suhendarweb@gmail.com>
2020-11-05 19:40:50 +0700

    File Assets

--- resources/views/universitas/mahasiswa.blade.php
+++ resources/views/universitas/mahasiswa.blade.php
@@ -4,28 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman View</title>
+    <link rel="stylesheet" href="/css/my-style.css">
 </head>
 <body>
     <h1>Daftar Mahasiswa</h1>
     <ol>
-<!--         
-        <li>Suhendar Aryadi</li>
+        
+        <!-- <li>Suhendar Aryadi</li>
         <li>Cici Alifa</li>
         <li>Burhan</li>
         <li>Wiwi Marwiyah</li> -->
 
-        <?php 
+        <?php
+
             foreach ($mahasiswa as $nama) {
                 echo "<li> $nama </li>";
             }
-        
         
         ?>
         
     </ol>
 
+    <!--  echo "$mahasiswa01"; ?> -->
+
+    <!-- <ul>
+    <li>Nama: < echo $nama; ?></li>
+    <li>Umur: < $umur; ?> </li>
+    <li>Kota Asal: < echo $kotaAsal; ?></li> -->
+    
+    
+    </ul>
+
+    <img src="/img/foto1.jpg">
+    <img src="/img/foto2.jpg" >
+    
+    <p>
     <div>
         Copyright <?php echo date("M-Y"); ?>. Suhendar Aryadi
     </div>
+
+    <script src="/js/my-script.js"></script>
 </body>
 </html>
